e4536f3b17c93bc17ac0c3582b73c39c2b2f0f18
Jefferson Murat <72477374+JrMurat12@users.noreply.github.com>
2023-04-12 17:47:27 -0300

    Commit pasta
    Commit pasta

---
+++ SuperDesafioAdimaxBolada/Data/DTOs/CreateProductDTO.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SuperDesafioAdimaxBolada.Data.DTOs;
+
+public class CreateProductDTO
+{
+    [Required(ErrorMessage = "O nome do produto é obrigatório")]
+    public string Name { get; set; }
+    [Required(ErrorMessage = "A descrição é obrigatória")]
+    public string Description { get; set; }
+    [Required]
+    public int Price { get; set; }
+    public int CategoryId { get; set; }
+}
---
+++ SuperDesafioAdimaxBolada/Models/Product.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SuperDesafioAdimaxBolada.Models;
+
+public class Product
+{
+    [Key]
+    [Required]
+    public int Id { get; set; }
+    [Required(ErrorMessage = "O nome do produto é obrigatório")]
+    public string Name { get; set; }
+    [Required(ErrorMessage = "A descrição é obrigatória")]
+    public string Description { get; set; }
+    [Required]
+    public int Price { get; set; }
+    public int CategoryId { get; set; }
+    public DateTime CreatedAt { get; set; } = DateTime.Now;
+    public string HasPendingLogUpdate { get; set; }
+
+}
