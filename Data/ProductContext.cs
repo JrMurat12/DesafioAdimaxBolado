0540c7a3f8077b2f6bb3d7561a789fed235dcd77
Jefferson Murat <72477374+JrMurat12@users.noreply.github.com>
2023-04-12 17:42:56 -0300

    Commit pasta
    Commit pasta

---
+++ Data/ProductContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.EntityFrameworkCore;
+using SuperDesafioAdimaxBolada.Models;
+using System.Collections.Generic;
+namespace SuperDesafioAdimaxBolada.Data;
+
+public class ProductContext : DbContext
+{
+    public ProductContext(DbContextOptions<ProductContext> opts)
+        : base(opts)
+    {
+
+    }
+
+    public DbSet<Product> Products { get; set; }
+}
---
+++ Profiles/ProductProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using SuperDesafioAdimaxBolada.Data.DTOs;
+using SuperDesafioAdimaxBolada.Models;
+
+namespace SuperDesafioAdimaxBolada.Profiles
+{
+    public class ProductProfile : Profile
+    {
+        public ProductProfile()
+        {
+            CreateMap<CreateProductDTO, Product>();
+            CreateMap<UpdateProductDTO, Product>();
+            CreateMap<Product, UpdateProductDTO>();
+            CreateMap<Product, ReadProductDTO>();
+        }
+    }
+}
